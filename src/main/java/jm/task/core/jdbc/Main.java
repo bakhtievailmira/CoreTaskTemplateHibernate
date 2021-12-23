78d067dcf2a6a8c521280820a8f6ab2bab24f963
bakhtievailmira <bakhtieva.ilmira@gmail.com>
2021-12-23 14:15:16 +0300

    Task for Kata Academy Hibernate v 1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -12,16 +12,16 @@ public class Main {
         UserService userService = new UserServiceImpl();
         List<User> usersList;
         userService.createUsersTable();
-        userService.saveUser("User1_Name","User1_LastName", (byte) 11);
-        userService.saveUser("User2_Name","User2_LastName", (byte) 22);
-        userService.saveUser("User3_Name","User3_LastName", (byte) 33);
-        userService.saveUser("User4_Name","User4_LastName", (byte) 44);
+        userService.saveUser("User1_Name", "User1_LastName", (byte) 11);
+        userService.saveUser("User2_Name", "User2_LastName", (byte) 22);
+        userService.saveUser("User3_Name", "User3_LastName", (byte) 33);
+        userService.saveUser("User4_Name", "User4_LastName", (byte) 44);
         usersList = userService.getAllUsers();
         for (int i = 0; i < usersList.size(); i++) {
             System.out.println(usersList.get(i));
         }
-    //    userService.cleanUsersTable();
-    //    userService.dropUsersTable();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
 
     }
 
